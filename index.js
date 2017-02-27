5f6cbb6ab522d891680a927a2beed51fe40730df
Kolosowski florent <kolosowski.florent@gmail.com>
2017-02-27 14:55:57 +0100

    typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function(env, methods) {
     methods = [methods];
 
   for (var i = 0; i < methods.length; i++)
-    env.methods[i] = env.methods[i].bind(env);
+    env[methods[i]] = env[methods[i]].bind(env);
 
   return env;
 };
