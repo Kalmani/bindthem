6a72348bea72f5062d6e8b2ff16480798ac63eb2
Kolosowski florent <kolosowski.florent@gmail.com>
2017-02-27 15:24:26 +0100

    add check on methods

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ module.exports = function(env, methods) {
   if (typeof methods == 'String')
     methods = [methods];
 
-  for (var i = 0; i < methods.length; i++)
+  for (var i = 0; i < methods.length; i++) {
+    if (!env[methods[i]]) continue;
     env[methods[i]] = env[methods[i]].bind(env);
+  }
 
   return env;
 };
